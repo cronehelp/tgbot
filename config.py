c2820952d021b8061e8ab1f96b8ffbfa7938a0a6
Klinok <klinok@li.ru>
2024-05-22 01:13:17 +0530

    Updated config for webhooks

--- config.py
+++ config.py
@@ -4,3 +4,15 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 if TOKEN is None:
     raise ValueError("Не удалось найти токен бота. Убедитесь, что вы правильно задали переменную среды TELEGRAM_BOT_TOKEN.")
+
+# Webhook settings
+YOUR_DOMAIN = os.getenv("YOUR_DOMAIN")
+YOUR_WEBHOOK_PATH = os.getenv("YOUR_WEBHOOK_PATH")
+
+if YOUR_DOMAIN is None or YOUR_WEBHOOK_PATH is None:
+    raise ValueError("Не удалось найти настройки вебхуков. Убедитесь, что вы правильно задали переменные среды YOUR_DOMAIN и YOUR_WEBHOOK_PATH.")
+
+WEBHOOK_URL = f"https://{YOUR_DOMAIN}/{YOUR_WEBHOOK_PATH}"
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = int(os.getenv("PORT", 3000))  # Используем порт из переменных среды или 3000 по умолчанию
+WEBHOOK_PATH = f'/{YOUR_WEBHOOK_PATH}'
--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 import asyncio
 import logging
 import sys
-from aiogram import Bot
+from aiohttp import web
+from aiogram import Bot, Dispatcher
 
-from config import TOKEN
+from config import TOKEN, WEBHOOK_URL, WEBAPP_HOST, WEBAPP_PORT, WEBHOOK_PATH
 from bot import dp
 from handlers.init import router
 
 from handlers.init import setup_handlers
 from utils.db import create_pool, create_tables
-from utils.broadcast import send_broadcast_message
 
-async def main() -> None:
+async def on_startup(app: web.Application) -> None:
     bot = Bot(TOKEN)
     dp.include_router(router)
     pool = await create_pool()  # Создание пула соединений с базой данных
     await create_tables(pool)  # Создание таблиц, если они еще не существуют
     setup_handlers(router, bot, pool)  # Передаем pool в setup_handlers
-    await dp.start_polling(bot)  # Запуск polling
-    await pool.close()  # Завершение работы с пулом соединений
+    await bot.set_webhook(WEBHOOK_URL)  # Устанавливаем вебхук
+    app['bot'] = bot
+    app['pool'] = pool
+
+async def on_shutdown(app: web.Application) -> None:
+    await app['bot'].delete_webhook()  # Удаляем вебхук при завершении работы
+    await app['bot'].session.close()
+    await app['pool'].close()
+
+async def handle(request: web.Request) -> web.Response:
+    bot = request.app['bot']
+    update = await request.json()
+    Dispatcher.set_current(dp)
+    await dp.feed_update(bot, update)
+    return web.Response()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+
+    app = web.Application()
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_post(WEBHOOK_PATH, handle)
+
+    web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT)

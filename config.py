6bf7e385192f156eafbd4d7c2a05e9ba8247ac8e
Klinok <klinok@li.ru>
2024-05-30 19:07:20 +0530

    Update project files

--- config.py
+++ config.py
@@ -1,18 +1,10 @@
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
 import os
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+load_dotenv()
 
-if TOKEN is None:
-    raise ValueError("Не удалось найти токен бота. Убедитесь, что вы правильно задали переменную среды TELEGRAM_BOT_TOKEN.")
-
-# Webhook settings
+TELEGRAM_BOT_TOKEN = TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 YOUR_DOMAIN = os.getenv("YOUR_DOMAIN")
 YOUR_WEBHOOK_PATH = os.getenv("YOUR_WEBHOOK_PATH")
-
-if YOUR_DOMAIN is None or YOUR_WEBHOOK_PATH is None:
-    raise ValueError("Не удалось найти настройки вебхуков. Убедитесь, что вы правильно задали переменные среды YOUR_DOMAIN и YOUR_WEBHOOK_PATH.")
-
-WEBHOOK_URL = f"https://{YOUR_DOMAIN}/{YOUR_WEBHOOK_PATH}"
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = int(os.getenv("PORT", 3000))
-WEBHOOK_PATH = f'/{YOUR_WEBHOOK_PATH}'
+DATABASE_URL = os.getenv("DATABASE_URL")
